a3b5735566f7153f991902d077bd8c00081ae9b6
Lucaasshq <lucashenriquebarros2016@gmail.com>
2025-11-09 15:35:36 -0300

    Tad Lista com array

--- src/Lista/Lista.java
+++ src/Lista/Lista.java
@@ -1,80 +1,72 @@
 package Lista;
 
-public class Lista implements ILista {
+public class Lista  {
+    private Object[] array;
+    private int size;
+
+    public Lista(int capacidade){
+        this.array = new Object[capacidade];
+        this.size = 0;
+    }
 
 
 
-    @Override
     public Object first() {
-        return null;
+        return array[0];
     }
 
-    @Override
     public Object last() {
-        return null;
+        return array[array.length-1];
     }
 
-    @Override
     public Object before(Object node) {
         return null;
     }
 
-    @Override
     public Object after(int n) {
         return null;
     }
 
-    @Override
     public int size() {
-        return 0;
+        return size;
     }
 
-    @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
-    @Override
     public boolean isFirst(int r) {
         return false;
     }
 
-    @Override
     public boolean isLast(int r) {
         return false;
     }
 
-    @Override
     public Object replaceElements(int n, Object o) {
         return null;
     }
 
-    @Override
     public void swapElements(int n, int j) {
 
     }
 
-    @Override
     public void insertBefore(int n, Object o) {
 
     }
 
-    @Override
     public void insertAfter(int n, Object o) {
 
     }
 
-    @Override
     public void insertFirst(Object o) {
 
     }
 
-    @Override
     public void insertLast(Object o) {
 
     }
 
-    @Override
     public Object remove(int n) {
         return null;
     }
