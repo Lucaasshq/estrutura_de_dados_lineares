4d96870a1a3650c0bfbf41653aa4055727dc5dbf
Lucaasshq <lucashenriquebarros2016@gmail.com>
2025-11-12 00:25:51 -0300

    listaarray

--- src/Lista/Lista.java
+++ src/Lista/Lista.java
@@ -85,11 +85,28 @@ public class Lista  {
         return o.equals(array[size-1]);
     }
     public Object replaceElements(int rank, Object o) {
-        return null;
+        if (isEmpty()){
+            throw new RuntimeException("Lista esta vazia");
+        }
+
+        if (rank < 0 || rank >= size){
+            throw new RuntimeException("Indice invalido");
+        }
+        Object elementoRemovido = array[rank];
+        array[rank] = o;
+        return elementoRemovido;
     }
 
     public void swapElements(int rank1, int rank2) {
-
+        if (isEmpty()){
+            throw new RuntimeException("Lista esta vazia");
+        }
+        if ((rank1 < 0 || rank1 >= size) || (rank2 < 0 || rank2 >= size)){
+            throw new RuntimeException("Indice invalido");
+        }
+        Object temp = array[rank1];
+        array[rank1] = array[rank2];
+        array[rank2] = temp;
     }
 
     public void insertBefore(int rank, Object o) {
@@ -141,7 +158,7 @@ public class Lista  {
         }
         Object elementoRemove = array[rank];
 
-        for (int i = rank; i < size; i++){
+        for (int i = rank; i < size-1; i++){
             array[i] = array[i+1];
         }
         size--;
